c85720c635fff26cf95ace58b63834ed7e882e0e
Avery King <kavery086@gmail.com>
2022-04-03 04:48:44 -0400

    dots

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {useEffect, useState} from 'react';
 
 export default function Home() {
+    const [textState, setTextState] = useState('');
+    useEffect(() => {
+        setInterval(() => {
+            const newVal = textState.length < 3 ? textState + '.' : '.';
+            setTextState(newVal)
+        },1000);
+    },[textState,setTextState])
   return (
-
       <>
           <Head>
               <title>Avery King</title>
           </Head>
           <h1 className={styles.title}>
-              Coming Soon...
+              Coming Soon <span className='dots'> {textState} </span>
           </h1>
       </>
 
